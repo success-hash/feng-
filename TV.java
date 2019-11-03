b3bec0399539f4fa03b58c3c6a683f8616c61b91
success-hash <54839590+success-hash@users.noreply.github.com>
2019-11-03 19:05:28 +0800

    Add files via upload

---
+++ TV.java
@@ -0,0 +1,10 @@
+public interface TV{
+public void play();}
+public class HaierTV implements TV{
+public void play(){
+System.out.println("This is Haier TV");
+}}
+public class HisenseTV implements TV{
+public void play(){
+System.out.println("This is a Hisense TV");
+}}
